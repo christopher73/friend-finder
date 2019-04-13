3e79c9655edbc8fbe30bf026ade2e951bf26153e
christopher fajardo <christopher.fajardo73@gmail.com>
2019-04-12 23:40:06 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,172 @@
+[
+  {
+    name: "Ahmed",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  {
+    name: "Jacob Deming",
+    photo:
+      "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+  },
+  {
+    name: "Jeremiah Scanlon",
+    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+  },
+  {
+    name: "Louis T. Delia",
+    photo:
+      "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
+  },
+  {
+    name: "Lou Ritter",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+  },
+  {
+    name: "Jordan Biason",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
+  },
+  {
+    name: "More testing",
+    photo: "https://www.bbc.com/news/world-latin-america-47907600",
+    scores: ["3", "2", "4", "2", "3", "2", "2", "3", "2", "2"]
+  },
+  {
+    name: "Joaquin",
+    photo:
+      "https://www.google.com/search?q=joaquin+phoenix&tbm=isch&source=iu&ictx=1&fir=o1wfF8I5Ro85_M%253A%252CKB_y3jyOp39AMM%252C%252Fm%252F018db8&vet=1&usg=AI4_-kR2AO2vXgNLRQlG3vYhvJh2Bw0CpQ&sa=X&ved=2ahUKEwjN7NXD88rhAhVKc98KHRONCMAQ_B0wJnoECAoQBg#imgrc=oraRAi99c7pucM:&vet=1",
+    scores: ["5", "1", "1", "3", "1", "1", "4", "1", "2", "1"]
+  },
+  {
+    name: "paris",
+    photo: "./Tala/jpg",
+    scores: ["2", "4", "3", "3", "3", "3", "3", "3", "4", "3"]
+  },
+  {
+    name: "mnbvc",
+    photo:
+      "https://postmediacalgaryherald2.files.wordpress.com/2018/03/unknown-15-jpeg.jpg?quality=80",
+    scores: ["1", "2", "3", "2", "3", "3", "3", "2", "2", "3"]
+  },
+  {
+    name: "mnbvc",
+    photo:
+      '"https://postmediacalgaryherald2.files.wordpress.com/2018/03/unknown-15-jpeg.jpg?quality=80"',
+    scores: ["1", "2", "3", "2", "3", "3", "3", "2", "2", "3"]
+  },
+  {
+    name: "papa",
+    photo:
+      "https://postmediacalgaryherald2.files.wordpress.com/2018/03/unknown-15-jpeg.jpg?quality=80",
+    scores: ["3", "3", "3", "3", "2", "4", "3", "4", "4", "3"]
+  },
+  {
+    name: "zouzou",
+    photo: ",jbgkxag",
+    scores: ["3", "1", "3", "3", "3", "2", "2", "4", "2", "4"]
+  },
+  {
+    name: "zouzou",
+    photo: ",jbgkxag",
+    scores: ["3", "1", "3", "3", "3", "2", "2", "4", "2", "4"]
+  },
+  {
+    name: "zouzou",
+    photo: ",jbgkxag",
+    scores: ["3", "1", "3", "3", "3", "2", "2", "4", "2", "4"]
+  },
+  {
+    name: "zouzou",
+    photo: ",jbgkxag",
+    scores: ["3", "1", "3", "3", "3", "2", "2", "4", "2", "4"]
+  },
+  {
+    name: "poopoo",
+    photo:
+      "https://www.google.com/search?rlz=1C5CHFA_enUS724US724&tbm=isch&q=image&chips=q:image,g_1:background:tDaUmIJOedo%3D&usg=AI4_-kSKjwgAuMN4IyDkxRhmWvIHHLfZBQ&sa=X&ved=0ahUKEwiAgpuqxMvhAhWqpFkKHeajAVcQ4lYIKCgA&biw=1412&bih=684&dpr=2#imgrc=R2_t7nzjZXGyEM:",
+    scores: ["4", "3", "2", "5", "3", "3", "3", "3", "3", "3"]
+  },
+  {
+    name: "John",
+    photo: "f",
+    scores: ["2", "3", "4", "4", "2", "1", "4", "2", "3", "2"]
+  },
+  {
+    name: "fdff",
+    photo: "f",
+    scores: ["2", "3", "2", "4", "3", "4", "1", "2", "2", "3"]
+  },
+  {
+    name: "g",
+    photo: "a",
+    scores: ["4", "3", "3", "3", "4", "5", "4", "4", "5", "3"]
+  },
+  {
+    name: "olya",
+    photo:
+      "https://www.facebook.com/photo.php?fbid=1886162124836084&set=a.113683562083958&type=3&theater",
+    scores: ["2", "2", "3", "1", "2", "2", "3", "4", "3", "4"]
+  },
+  {
+    name: "olya",
+    photo:
+      "https://www.facebook.com/photo.php?fbid=1886162124836084&set=a.113683562083958&type=3&theater",
+    scores: ["2", "2", "3", "4", "2", "2", "3", "4", "3", "4"]
+  },
+  {
+    name: "dfggssg",
+    photo: "sdgd",
+    scores: ["4", "4", "1", "3", "3", "3", "3", "3", "3", "2"]
+  },
+  {
+    name: "Kathy Chen",
+    photo: "https://imgur.com/U88zL9d",
+    scores: ["5", "2", "4", "1", "5", "1", "5", "1", "2", "5"]
+  },
+  {
+    name: "Kathy Chen",
+    photo: "https://imgur.com/U88zL9d",
+    scores: ["5", "2", "4", "1", "5", "1", "5", "1", "2", "5"]
+  },
+  {
+    name: "Kathy Chen",
+    photo: "https://imgur.com/U88zL9d",
+    scores: ["5", "2", "4", "1", "5", "1", "5", "1", "2", "5"]
+  },
+  {
+    name: "Nick",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png",
+    scores: ["2", "4", "2", "2", "4", "3", "2", "5", "1", "2"]
+  },
+  {
+    name: "Kathy Chen",
+    photo: "https://imgur.com/U88zL9d",
+    scores: ["5", "2", "4", "1", "5", "1", "5", "1", "2", "5"]
+  },
+  {
+    name: "Leonardo",
+    photo: "https://images.app.goo.gl/zYVy8cdwsWYmnvic8",
+    scores: ["4", "4", "1", "1", "1", "3", "4", "2", "1", "5"]
+  },
+  {
+    name: "xxx",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+  },
+  {
+    name: "cfr",
+    photo:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.geek.com%2Fwp-content%2Fuploads%2F2018%2F07%2Fconfusedsaitama1-625x352.png&imgrefurl=https%3A%2F%2Fwww.geek.com%2Fanime%2Fwhen-can-we-expect-to-see-one-punch-man-season-2-1745435%2F&docid=Uu3L2GLeK6LRSM&tbnid=pDxa6_L4Pc8w4M%3A&vet=10ahUKEwjrsd2BjczhAhVpRN8KHZgSCuMQMwhqKAIwAg..i&w=625&h=352&bih=789&biw=1440&q=one%20punch%20man&ved=0ahUKEwjrsd2BjczhAhVpRN8KHZgSCuMQMwhqKAIwAg&iact=mrc&uact=8",
+    scores: ["5", "1", "3", "4", "4", "4", "4", "4", "1", "4"]
+  }
+];
